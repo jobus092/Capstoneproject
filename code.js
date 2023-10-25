859046790e888ade8966cb5a8adf3644004b292f
jobus092 <147642202+jobus092@users.noreply.github.com>
2023-10-25 05:56:21 +0000

    help bruh

--- code.js
+++ code.js
@@ -6,10 +6,10 @@ function moFunction(){
 
 const myTimeout = setTimeout(timer, 2000);
 
-function timer() {
+function timer(){
   document.getElementById("timeout").style.backgroundColor="blue"
 }
 
-function inner() {
+function inner(){
     document.getElementById("iHtml").innerHtml="I live in Norway and I don't go to school. I hope to one day become a racecar driver";
 }
