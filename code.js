1547495e351c084de854bd261c87ef9181998579
jobus092 <147642202+jobus092@users.noreply.github.com>
2023-10-25 05:32:09 +0000

    added settimout

--- code.js
+++ code.js
@@ -3,3 +3,9 @@
 function moFunction(){
     document.getElementById("AboutMeText").style.backgroundColor="red"
 }
+
+const myTimeout = setTimeout(timer, 5000);
+
+function timer() {
+  document.getElementById("setTimeout").style.backgroundColor="purple"
+}
