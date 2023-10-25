a97f1bfbdb44d7ede02214f751339455fe43e7cf
jobus092 <147642202+jobus092@users.noreply.github.com>
2023-10-25 06:03:56 +0000

    test

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ function moFunction(){
     document.getElementById("AboutMeText").style.backgroundColor="red"
 }
 
-const myTimeout = setTimeout(timer, 2000);
+const myTimeout = setTimeout(timer, 5000);
 
 function timer(){
   document.getElementById("timeout").style.backgroundColor="blue"
